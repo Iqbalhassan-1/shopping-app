2bbcbcac0ac0d7eecc704b036833124389de2326
Iqbal Hassan <107492954+Emiqbal11@users.noreply.github.com>
2022-12-07 19:29:43 +0500

    all will be done also payment and order show the strapi dashboard

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,42 +1,65 @@
 import React from "react";
 import "./Cart.scss";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
-import {useSelector,useDispatch} from 'react-redux'
-import { removeItem,resetCart } from "../../redux/cartReducer"
-
-
+import { useSelector, useDispatch } from "react-redux";
+import { removeItem, resetCart } from "../../redux/cartReducer";
+import { loadStripe } from "@stripe/stripe-js";
+import { makeRequest } from "../../makeRequest";
 const Cart = () => {
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
 
-  const products=useSelector(state=>state.cart.products);
+  const products = useSelector((state) => state.cart.products);
 
-  const TotalPrice=()=>{
-    let total=0;
-    products.forEach(item=>{
-      total+=item.quantity*item.price;
+  const TotalPrice = () => {
+    let total = 0;
+    products.forEach((item) => {
+      total += item.quantity * item.price;
     });
     return total.toFixed(2);
-  }
+  };
+  const stripePromise = loadStripe(
+    "pk_test_51M1NojGmnAE20fMhVWnhPRRnRK201yc0Vu9frVjkuCTekVMnMYbUfNhClN4w6CmWzhvO6GZMb3nbT8daXDhnF4NY00K78KCGjQ"
+  );
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res=await makeRequest.post("/orders",{
+        products
+      });
+      await stripe.redirectToCheckout({
+        sessionId:res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="cart">
       <h1>Products in your cart</h1>
       {products?.map((item) => (
         <div className="item" key={item.id}>
-          <img src={process.env.REACT_APP_UPLOAD_URL+item.img} alt="" />
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
           <div className="details">
             <h1>{item.title}</h1>
             <p>{item.desc?.substring(0, 100)}</p>
-            <div className="price">{item.quantity}x${item.price}</div>
+            <div className="price">
+              {item.quantity}x${item.price}
+            </div>
           </div>
-          <DeleteOutlinedIcon className="delete" onClick={()=>dispatch(removeItem(item.id))} />
+          <DeleteOutlinedIcon
+            className="delete"
+            onClick={() => dispatch(removeItem(item.id))}
+          />
         </div>
       ))}
       <div className="total">
         <span>SUBTOTAL</span>
         <span>${TotalPrice()}</span>
       </div>
-      <button>PROCEED TO CHECKOUT</button>
-      <span className="reset" onClick={()=>dispatch(resetCart())}>Reset Cart</span>
+      <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      <span className="reset" onClick={() => dispatch(resetCart())}>
+        Reset Cart
+      </span>
     </div>
   );
 };
